324bd24e8f321db5a49199383675163985c75318
Joshua Eversole <josh@eversoles.org>
2025-07-30 17:28:43 +0800

    Finished Baseline setup

--- main.go
+++ main.go
@@ -1,9 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
-	fmt.Println("This is a simple Go program.")
-	fmt.Println("Let's learn Go together!")
+	godotenv.Load() // Load environment variables from .env file
+
+	portString := os.Getenv(".env")
+	if portString == "" {
+		log.Fatal("PORT environment variable is not set")
+	}
+
+	router := chi.NewRouter()
+	// Using the router handler and the port from the environment variable
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	fmt.Println("Server will run on port:", portString)
+
+	// Should skip this if everything runs correctly
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatalf(err)
+	}
+
 }
